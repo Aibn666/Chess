c6681f0c6357155ef2cf7c9a73bb3bd6b6c46329
Aibn666 <ivan.krysiuk.93@gmail.com>
2022-02-24 15:23:20 -0300

    Queen movement

--- src/clases/piezas/Queen.ts
+++ src/clases/piezas/Queen.ts
@@ -1,9 +1,40 @@
 import { PieceType } from '../../types/theme';
 import Piece from "../piezas";
+import Cell from '../celdas';
 
 class Queen extends Piece {
     constructor(color){
         super(color,PieceType.queen,['♕','♛']);
+    }
+    availableMovement(position: [number, number], boardMatrix: Cell[][]){
+        const directions = [
+            [1, 1 ],
+            [-1, 1],
+            [1, -1],
+            [-1,-1],
+            [0, -1],
+            [1, 0 ],
+            [0, 1 ],
+            [-1, 0],
+        ];
+
+        directions.forEach((dir) =>  this.checkDirection(position,dir, boardMatrix));
+        //down right
+        //this.checkDirection(position, [1, 1], boardMatrix);
+        //down left
+        //this.checkDirection(position, [-1, 1], boardMatrix);
+        //up right
+        //this.checkDirection(position, [1, -1], boardMatrix);
+        //up left
+        //this.checkDirection(position, [-1, -1], boardMatrix);
+        //up
+        //this.checkDirection(position, [0, -1], boardMatrix);
+        //right
+        //this.checkDirection(position, [1, 0], boardMatrix);
+        //down
+        //this.checkDirection(position, [0, 1], boardMatrix);
+        //left
+        //this.checkDirection(position, [-1, 0], boardMatrix);
     }
 }
 
