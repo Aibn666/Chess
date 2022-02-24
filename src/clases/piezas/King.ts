51df716fa6ee32e519cb7a1c671c68d0d4d06d8a
Aibn666 <ivan.krysiuk.93@gmail.com>
2022-02-24 15:32:21 -0300

    king movement

--- src/clases/piezas/King.ts
+++ src/clases/piezas/King.ts
@@ -1,9 +1,29 @@
 import { PieceType } from '../../types/theme';
 import Piece from "../piezas";
+import Cell from '../celdas';
 
 class King extends Piece {
     constructor(color){
         super(color,PieceType.king,['♔','♚']);
+    }
+    availableMovement(position: [number, number], boardMatrix: Cell[][]){
+        const [x, y] = position;
+        const directions = [
+            [1, 1 ],
+            [-1, 1],
+            [1, -1],
+            [-1,-1],
+            [0, -1],
+            [1, 0 ],
+            [0, 1 ],
+            [-1, 0],
+        ];
+
+        directions.forEach((dir)=>{
+            const [xDir, yDir] = dir;
+            const cell = this. getCellFromCoords([x + (1*xDir), y + (1*yDir)], boardMatrix);
+            if (cell && !(cell.piece && cell.piece.color == this.color)) cell.setAvailableMovement(true);
+        });
     }
 }
 
